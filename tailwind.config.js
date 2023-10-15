3b2ffcd690a0b67387a007af44239dcdc310ebf9
julius garalevicius <juliusgaralevicius@gmail.com>
2023-10-15 13:05:55 +0300

    update everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-strong-cyan': '#26baa4',
+        'primary-light-blue': '#6173ff',
+        'neutral-dark-grayish-blue': '#4c545d',
+        'neutral-grayish-blue': '#9fabb2',
+      },
+      fontFamily: {
+        'ba-jamjuree': ['"Bai Jamjuree", "sans-serif"'],
+      },
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
+      },
+    },
   },
   plugins: [],
 }
-
